8ff1b0df3d5d78a5d1767dbf60d7b5791a7f5172
unknown <joshuaho1@cpp.edu>
2023-11-19 16:15:20 -0800

    Implemented React Router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 import "./styles.css";
 
-import App from "./App";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import TicTacToe from "./pages/TicTacToe";
+import API from "./pages/API";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path = "/" element = {<Layout />}>
+          <Route index element ={<Home />}/>
+          <Route path = "api" element = {<API/>}/>
+          <Route path = "tictactoe" element = {<TicTacToe/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
 
 const root = createRoot(document.getElementById("root"));
 root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <App />
 );
---
+++ src/pages/API.js
@@ -0,0 +1,5 @@
+const API = () => {
+    return <h1>APi</h1>
+}
+
+export default API; // Can do this instead of exporting from function
