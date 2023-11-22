350bd3b023dcef7bb127c46f82c161bba5730749
jhhocs <joshuaho1@cpp.edu>
2023-11-21 21:03:00 -0800

    Implemented Dictionary API tab

--- src/pages/API.js
+++ src/pages/API.js
@@ -1,5 +1,57 @@
-const API = () => {
-    return <h1>APi</h1>
-}
+import React, { useState } from "react";
 
-export default API; // Can do this instead of exporting from function
+export default function Search() {
+    const [word, setWord] = useState("");
+    const [responses, setResponses] = useState(null);
+    const [hasResponse, setHasReseponse] = useState(false);
+
+    const fetchDefinition = async () => {
+        try {
+            const response = await (await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)).json()
+            setResponses(response);
+            setHasReseponse(true);
+        }
+        catch (err){
+            console.log(err.message);
+        }
+    }
+
+    function Response() {
+        if(!hasResponse) {
+            return;
+        }
+        else {
+            return (
+                <div>
+                    {responses.map((response) => (
+                        <div>
+                            <h1>{response.word}</h1>
+                            <p>{response.phonetic}</p>
+                            {response.meanings.map((meaning) => (
+                                <div>
+                                    <p><i>{meaning.partOfSpeech}</i></p>
+                                    <ol>
+                                        {meaning.definitions.map((definition) => (
+                                            <li>{definition.definition}</li>
+                                        ))}
+                                    </ol>
+                                    
+                                </div>
+                            ))}
+
+                        </div>
+                    ))}
+                </div>
+            );
+        }
+    }
+    
+    return (
+        <>
+            <h1>Dictionary API</h1>
+            <input required="required" placeholder = "Enter word here" value = {word} onChange={(e) => setWord(e.target.value)}/>
+            <button type = "submit" onClick = {fetchDefinition}>Search</button>
+            <Response />
+        </>
+    );
+  }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,12 @@
 export default function Home() {
     return (
-        <h1>Home</h1>
+        <div>
+            <h1>Home</h1>
+            <h3>Welcome!</h3>
+            <p>Click on the API link to find the definitions of words with Dictionary API</p>
+            <p>Click on the Tic Tac Toe tab to play Tic Tac Toe</p>
+        </div>
+        
+        
     )
 }
