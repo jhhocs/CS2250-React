8fdce0040a4c510731c7e267bae4d23b0664952d
jhhocs <joshuaho1@cpp.edu>
2023-11-21 21:54:19 -0800

    Fixed router

--- src/index.js
+++ src/index.js
@@ -12,13 +12,13 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path = "/CS2250-React" element = {<Layout />}>
+          <Route path = "/CS2250-React" element = {<Layout />}>
           <Route index element ={<Home />}/>
           <Route path = "api" element = {<API/>}/>
           <Route path = "tictactoe" element = {<TicTacToe/>}/>
         </Route>
       </Routes>
-    </BrowserRouter>
+    </BrowserRouter>  
   )
 }
 
